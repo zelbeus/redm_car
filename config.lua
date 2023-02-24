ef15784c83c2dde3c0ae0579b6f03faa38924cdb
ricx <66686454+zelbeus@users.noreply.github.com>
2023-02-24 14:31:22 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,5 +5,5 @@ Config.SpawnCommand = true
 Config.Command = "spawn_car"
 Config.DeleteCommand = "delete_car"
 Config.CarSpawns = {
-    {pos = {2454.492, -1319.561, 45.433}, spawn = {x = 2447.080, -1321.614, 45.405, 95.208}}
+    {pos = {2454.492, -1319.561, 45.433}, spawn = {x = 2447.080, y = -1321.614, z =  45.405, h = 95.208}}
 }
